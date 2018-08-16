724aeefc9b3ca51af8955d74ccd543c1732a7ead
Todd M <tboogie87@me.com>
2018-08-16 14:49:50 -0400

    added JOIN to tables workouts and users

--- db/schema.sql
+++ db/schema.sql
@@ -1,28 +1,33 @@
-DROP DATABASE IF EXISTS fitness_app;
 CREATE DATABASE fitness_app;
+-- DROP DATABASE IF EXISTS fitness_app;
 
 \c fitness_app
 
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS workouts;
 
-CREATE TABLE workouts (
-  id SERIAL PRIMARY KEY,
-  title TEXT,
-  body TEXT
-);
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   reps INTEGER,
   first_name TEXT,
   last_name TEXT,
-  type_of_pushup TEXT,
-  workout_id INTEGER REFERENCES workouts(id)
+  type_of_pushup TEXT
 );
 
+CREATE TABLE workouts (
+  id SERIAL PRIMARY KEY,
+  title TEXT,
+  user_id INTEGER REFERENCES users
+);
 --build CRUD models joining both tables
 --refer to the blog app from class
 --cd into folder with schema and seed files
 --run psql -f schema.sql;
 --run psql -f seed.sql;
+
+-- join tables..
+-- SELECT * 
+-- FROM workout 
+-- JOIN users 
+-- ON workouts.user_id = users.id;
