5d181d9da45b1fc92e50476ed1ade67311fb1a88
Todd M <tboogie87@me.com>
2018-08-16 14:50:31 -0400

    added user_id

--- db/seed.sql
+++ db/seed.sql
@@ -1,24 +1,18 @@
 \c fitness_app
 
--- Correct posts to userss table
+-- Correct posts to users table
 -- Don't insert id (id SERIAL PRIMARY KEY)
 
-INSERT INTO workouts (title, body) 
-VALUES (
-    'closePushUps', 
-    'This variation of push ups works directly on your triceps and the presternal region (inner chest)'
-); 
-INSERT INTO workouts (title, body) 
-VALUES ( 
-    'neutralPushUps', 
-    'This variation of push ups works more on your pectoral and deltoid region'
-);
-INSERT INTO workouts (title, body) 
-VALUES (
-    'widePushUps', 
-    'Lastly, this variation of push ups builds on top of the pectoral region but also adds to the greater supraclavicular fossa (outer part of the chest)'
-);
+INSERT INTO workouts (title, user_id) 
+    VALUES ('closePushUps', 1); 
+INSERT INTO workouts (title, user_id) 
+    VALUES ( 'neutralPushUps', 2);
+INSERT INTO workouts (title, user_id) 
+    VALUES ('widePushUps', 3);
 
-INSERT INTO users (reps, first_name, last_name, type_of_pushup, workout_id) VALUES (10, 'Aubrey', 'Johnson','closePushUps', 1);
-INSERT INTO users (reps, first_name, last_name, type_of_pushup, workout_id) VALUES (20, 'Glory', 'Johnson', 'neutralPushUps', 2);
-INSERT INTO users (reps, first_name, last_name, type_of_pushup, workout_id) VALUES (30, 'Kimmie', 'Yearwood', 'widePushUps', 3);
+INSERT INTO users (reps, first_name, last_name, type_of_pushup) 
+    VALUES (10, 'Aubrey', 'Johnson','closePushUps');
+INSERT INTO users (reps, first_name, last_name, type_of_pushup) 
+    VALUES (20, 'Glory', 'Johnson', 'neutralPushUps');
+INSERT INTO users (reps, first_name, last_name, type_of_pushup) 
+    VALUES (30, 'Kimmie', 'Yearwood', 'widePushUps');
